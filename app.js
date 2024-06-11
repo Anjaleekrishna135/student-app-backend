fdfb9d4116744451e7dba890b666080066a6d879
Anjaleekrishna135 <anjaleekrishna135@gmail.com>
2024-06-11 10:22:39 +0530

    changes

--- app.js
+++ app.js
@@ -1,19 +1,32 @@
 const express = require("express")
-const mongoose =require("mongoose")
-const cors =require("cors")
-
+const mongoose = require("mongoose")
+const cors = require("cors")
+const { studentmodel } = require("./models/student")
 const app = express()
-app.use(cors())
 
-app.get("/",(req,res)=>{
+app.use(cors())
+app.use(express.json())
+
+app.get("/", (req, res) => {
     res.send("hello")
 })
 
-app.get("/contact",(req,res)=>{
+app.get("/contact", (req, res) => {
     res.send("welcome to my contact page")
 })
 
-app.listen(8080,()=>{
+
+
+app.post("/add", (req, res) => {
+    let input = req.body
+    console.log(input)
+    res.send("test")
+})
+
+app.listen(8080, () => {
     console.log("server started")
 
 })
+
+
+
