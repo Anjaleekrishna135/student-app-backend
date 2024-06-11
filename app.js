9225df47fec7ecbb23880d7407562e645aa42b21
Anjaleekrishna135 <anjaleekrishna135@gmail.com>
2024-06-11 12:40:44 +0530

    changes

--- app.js
+++ app.js
@@ -6,7 +6,20 @@ const { studentmodel }=require("./models/student")
 const app=express()
 app.use(cors())
 app.use(express.json())
+mongoose.connect("mongodb+srv://anjaleekrishnaps2001:anjalee2001@cluster0.cnbaf.mongodb.net/studentdb?retryWrites=true&w=majority&appName=Cluster0")
 
+app.post("/add", (req, res) => {
+    let input = req.body
+    let student=new studentmodel(input)
+    student.save()
+    console.log(student)
+    res.json({"status":"success"})
+})
+
+app.listen(8080, () => {
+    console.log("server started")
+
+})
 app.get("/", (req, res) => {
     res.send("hello")
 })
@@ -15,19 +28,6 @@ app.get("/contact", (req, res) => {
     res.send("welcome to my contact page")
 })
 
-
-
-app.post("/add", (req, res) => {
-    let input = req.body
-    let student=new studentmodel(input)
-    console.log(student)
-    res.send("test")
-})
-
-app.listen(8080, () => {
-    console.log("server started")
-
-})
 
 
 
